83b04e5e57c1ebf1e16f352bcd860736384aff93
mialeo <marc@enjolras.fr>
2024-03-08 11:54:55 +0100

    Modification pour avoir toutes les URL d'un coup et rapidement plutot que de passe beaucoup de temps à parser chaque URL

--- ExtractYoutubeVideosList.py
+++ ExtractYoutubeVideosList.py
@@ -6,18 +6,25 @@ os.chdir('C:/Users/micro/IA')
 # URL de la playlist YouTube
 playlist_url = 'https://www.youtube.com/@Antoine_Peytavin_Officiel/videos'
 
-# Utiliser yt-dlp pour obtenir les informations sur la playlist
-with yt_dlp.YoutubeDL() as ydl:
-    playlist_info = ydl.extract_info(playlist_url, download=False)
+# Configuration pour yt-dlp
+ydl_opts = {
+    'extract_flat': True,
+    'quiet': True,
+    'force_generic_extractor': True,
+    'skip_download': True,
+}
 
-# Extraire uniquement les URL des vidéos de la playlist
-video_urls = [entry['webpage_url'] for entry in playlist_info['entries']]
+# Utiliser yt-dlp pour obtenir les informations sur la chaîne
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    channel_info = ydl.extract_info(channel_url)
+
+# Extraire les URL des vidéos avec le préfixe ajouté
+video_urls = [f"https://www.youtube.com/watch?v={entry['id']}" for entry in channel_info['entries']]
 
 # Créer un répertoire avec le nom de la chaîne YouTube
 os.makedirs(playlist_info['title'], exist_ok=True)
 
-# Ecrit la liste dans le fichier qui poste son nom
+# Écrire les URLs dans un fichier
 with open(f"./{playlist_info['title']}/url.lst", "w", encoding="utf-8") as file:
     for url in video_urls:
         file.write(f"{url}\n")
-
